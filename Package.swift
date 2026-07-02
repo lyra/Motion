6e96413afa3e5d771d3a1f4500552df1b52a0fa6
MageSantana <33928559+MageSantana@users.noreply.github.com>
2026-07-02 16:03:37 +0200

    Update Package.swift
    Set min iOS to 15.0
    
    Signed-off-by: MageSantana <33928559+MageSantana@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 5.4
 import PackageDescription
 
 let package = Package(
     name: "LyraMotion",
     platforms: [
-        .iOS(.v26)
+        .iOS(.v15)
     ],
     products: [
         .library(
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "LyraMotion",
-            url: "https://raw.githubusercontent.com/lyra/Motion/4.0.4/LyraMotion.xcframework.zip",
+            url: "https://raw.githubusercontent.com/lyra/Motion/4.0.5/LyraMotion.xcframework.zip",
             checksum: "a6230ff0a0e8b596c6bad44a51c62af5db0dc9565fe6239e35545df2027645bf"
         )
     ]
